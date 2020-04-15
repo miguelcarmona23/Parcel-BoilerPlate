2da95e9eca5419fc45167b3604763699c2b25a29
Miguel Carmona <miguelcarmona996@gmail.com>
2020-04-15 13:52:16 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import User from './components/User';
+import Header from './components/Header';
+import './scss/app.scss';
+
+const app = async() => {
+    document.getElementById('header').innerHTML = Header();
+    document.getElementById('user').innerHTML = await User();
+};
+
+// Init app
+app();
